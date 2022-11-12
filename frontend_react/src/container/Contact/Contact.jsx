fd5a4e6e8afc8b572374ea016765d3740d7f5285
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-11-12 10:28:13 -0500

    fix form post request
    - form name incorrect in payload
    -- change "contact-demo" to "contact"

--- frontend_react/src/container/Contact/Contact.jsx
+++ frontend_react/src/container/Contact/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
         fetch("/", {
           method: "POST",
           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact-demo", ...values })
+          body: encode({ "form-name": "contact", ...values })
         })
           .then(() => {
             alert(JSON.stringify(values));
